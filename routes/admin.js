372724fad939bddec722fca4e28ff899ca23140a
Travis Mix <travismix1980@gmail.com>
2020-10-02 18:39:31 -0700

    Initial commit

---
+++ routes/admin.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const path = require('path');
+
+router.get('/add-product',(req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+});
+
+router.post('/add-product', (req, res) => {
+    console.log(req.body.title);
+    res.redirect('/');
+});
+
+module.exports = router;
