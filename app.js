3042c36fa5c129b9b85a0f1493ba21733c1c2290
Travis Mix <travismix1980@gmail.com>
2020-10-02 18:59:21 -0700

    modified listen to console log a link to click to open site during dev

--- app.js
+++ app.js
@@ -14,4 +14,6 @@ app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Sever listening at http://localhost:3000');
+});
