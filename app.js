ded72607b114fbb52f9f873ca8207ecbbe9b2863
Travis Mix <travismix1980@gmail.com>
2020-10-02 18:47:45 -0700

    Hopefully node_modules removed

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
